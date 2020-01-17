46be8438f93b3993fb1e80b513ef440a76103d69
Armando Roman <armandoroman1016@gmail.com>
2020-01-16 20:03:37 -0800

    event modal styled

--- party_planner/src/components/ConfirmDelete.js
+++ party_planner/src/components/ConfirmDelete.js
@@ -4,12 +4,14 @@ import { deleteEvent } from '../actions/eventActions'
 import { connect } from 'react-redux'
 import { Button, Confirm } from 'semantic-ui-react';
 
-const ConfirmDelete = ({ deleteEvent, eventId }) => {
+const ConfirmDelete = ({ deleteEvent, eventId, handleEventMenu }) => {
 
   const history = useHistory()
   const [confirmOpen, setConfirmOpen] = useState(false);
-
+  
   const handleConfirmOpen = () => {
+    handleEventMenu(false)
+    // closes the event settings menu on click
     setConfirmOpen(true);
   }
 
@@ -25,15 +27,19 @@ const ConfirmDelete = ({ deleteEvent, eventId }) => {
   }
 
   return (
-      <div>
+      <div className = 'confirm-delete'>
         <p onClick={handleConfirmOpen}>Delete Event</p>
         <Confirm
           content="Deleting an event is irreversible. Do you want to delete this event?"
-          confirmButton={<Button style={{backgroundColor: 'rgb(208, 17, 31)'}}>Delete</Button>}
+          cancelButton = "Never mind"
+          confirmButton={<Button style={{backgroundColor: '#E3696A'}}>Delete</Button>}
           open={confirmOpen}
           onCancel={handleConfirmClose}
           onConfirm={() => handleDel(eventId)} 
           color="red"
+          className = 'confirm'
+          size = 'large'
+          id = 'delete-confirm'
         />
       </div>
   )
